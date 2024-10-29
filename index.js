9def120a9208e85bd760e231344132ac9bd97c3d
vigneshherao <vigneshfornavy@gmail.com>
2024-10-29 14:28:08 +0530

    Added router for handle route

--- index.js
+++ index.js
@@ -2,41 +2,15 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const connectDb = require("./config/database");
-const adminModel = require("./models/admin");
-const adminAuth = require("./middleware/adminAuth");
 const cookieParser = require("cookie-parser");
+const admin = require("./routes/auth");
 
 //middleware
 app.use(express.json());
 app.use(cookieParser());
 
-app.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const isValidAdmin = await adminModel.findOne({ email });
-
-    if (!isValidAdmin) {
-      throw new Error("admin is not valid");
-    }
-
-    const isValidPassword = await isValidAdmin.comparePassword(password);
-
-    if (!isValidPassword) {
-      throw new Error("password is not valid");
-    }
-
-    const token = await isValidAdmin.getJwt();
-
-    res.cookie("token", token, {
-      expires: new Date(Date.now() + 2 * 24 * 60 * 60 * 1000),
-    });
-
-    res.status(200).json({ message: "Admin logged in successfully" });
-  } catch (error) {
-    res.status(500).json({ error: "Error occurred: " + error.message });
-  }
-});
+//routes
+app.use("/admin", admin);
 
 //Database connection
 connectDb()
---
+++ routes/auth.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const adminModel = require("../models/admin");
+const router = express.Router();
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const isValidAdmin = await adminModel.findOne({ email });
+
+    if (!isValidAdmin) {
+      throw new Error("admin is not valid");
+    }
+
+    const isValidPassword = await isValidAdmin.comparePassword(password);
+
+    if (!isValidPassword) {
+      throw new Error("password is not valid");
+    }
+
+    const token = await isValidAdmin.getJwt();
+
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 2 * 24 * 60 * 60 * 1000),
+    });
+
+    res.status(200).json({ message: "Admin logged in successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Error occurred: " + error.message });
+  }
+});
+
+module.exports = router;
