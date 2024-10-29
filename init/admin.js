ca8fe60d3035e446b885a001a8d7ea52b95d0c44
vigneshherao <vigneshfornavy@gmail.com>
2024-10-29 12:42:39 +0530

    Added schema validation for admin and created a admin

--- init/admin.js
+++ init/admin.js
@@ -1,18 +1,19 @@
-const mongoose = require("mongoose");
 const adminModel = require("../models/admin");
 const connectDb = require("../config/database");
+const bcrypt = require("bcrypt");
 
 const addAdmin = async () => {
   try {
     await connectDb();
+    await adminModel.deleteMany({});
     const adminObj = {
       email: "admin@gmail.com",
       password: "admin@2024",
     };
-
+    const hasedPassword = await bcrypt.hash(adminObj.password, 10);
+    adminObj.password = hasedPassword;
     const data = await new adminModel(adminObj).save();
-
-    console.log("admin is created" + data);
+    console.log("Admin created sucessfully " + data.email);
   } catch (error) {
     console.log("Error in creating admin" + error.message);
   }
--- models/admin.js
+++ models/admin.js
@@ -1,10 +1,18 @@
 const mongoose = require("mongoose");
 const { type } = require("os");
+const validator = require("validator");
+const { default: isEmail } = require("validator/lib/isEmail");
 
 const adminSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
+    validate(value) {
+      if (!isEmail(value)) {
+        throw new Error("Please give valid email");
+      }
+    },
   },
   password: {
     type: String,
